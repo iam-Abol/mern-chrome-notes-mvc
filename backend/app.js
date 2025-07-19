e222896810643dd2370dd6dbb4d2f8d44865453f
unknown <mh.z77w@gmail.com>
2025-07-19 18:17:35 +0330

    commit

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,7 @@ const compression = require("compression");
 
 const authMiddleware = require("./middlewares/auth");
 const flash = require("connect-flash");
+
 /////
 /////
 /////
@@ -40,16 +41,24 @@ app.use(
   })
 );
 app.use(flash());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   res.locals.username = req.session.username || null;
   res.locals.error = req.flash("error") || null;
+
   next();
 });
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(authRoutes);
 app.use(noteRoutes);
+app.get("/debug", (req, res) => {
+  res.send({
+    csrfToken: req.csrfToken(),
+    session: req.session,
+  });
+});
+
 app.use("/", authMiddleware, (req, res, next) => {
   res.render("404", { title: "page not found" });
 });
@@ -59,7 +68,8 @@ app.listen(3000, () => {
 app.use((err, req, res, next) => {
   res.render("error", {
     title: "ERROR",
-    message: err.message,
-    stack: err.stack,
+    // message: err.message,
+    message: "",
+    stack: "",
   });
 });
